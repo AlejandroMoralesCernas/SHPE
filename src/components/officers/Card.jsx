b34a762c21c9713873308cede629ac208b3d60a9
AlejandroMorales2003 <moralea3@oregonstate.edu>
2023-09-24 15:36:08 -0700

    fixed responsive design for iPhoneSE

--- src/components/officers/Card.jsx
+++ src/components/officers/Card.jsx
@@ -5,14 +5,16 @@ import Test from "../../data/images/officers/duck.jpeg"
 const Card = (props) => {
     console.log(props.image)
     return (
-        <div name="Officer Container" className='flex flex-col bg-[#D3D3D3] rounded-2xl md:h-[40rem] md:w-[20rem] w-[90vw] h-[70vh] items-center justify-start gap-[.5rem] shadow-md text-center'>
+        <div name="Officer Container" className='flex flex-col bg-[#D3D3D3] rounded-2xl md:h-[40rem] md:w-[20rem] w-[90vw] h-[70vh] items-center justify-start shadow-md text-center'>
             <img src={Test} alt="officer img" className="object-cover h-[70%] w-[100%] rounded-t-2xl" />
-            <h1 className='2xl:text-[1.2rem] text-[1rem] font-semibold'>{props.officer}</h1>
-            <h2 className='2xl:text-[1.2rem] text-[.975rem]'>{props.position}</h2>
-            <h3 className='2xl:text-[1rem] text-[.9rem] px-[.5rem]'>{props.major}</h3>
-            <a href={props.linkedin}>
-                <FaLinkedin className="text-[2.8rem] hover:scale-[105%] duration-300" /> 
-            </a>
+            <div name='text container' className='flex flex-col justify-center items-center mt-[.5rem] gap-[.5rem]'>
+                <h1 className='2xl:text-[1.2rem] text-[1rem] font-semibold'>{props.officer}</h1>
+                <h2 className='2xl:text-[1.2rem] text-[.975rem]'>{props.position}</h2>
+                <h3 className='2xl:text-[1rem] text-[.9rem] px-[.5rem]'>{props.major}</h3>
+                <a href={props.linkedin}>
+                    <FaLinkedin className="text-[2rem] sm:text-[2.8rem] hover:scale-[105%] duration-300" /> 
+                </a>
+            </div>
         </div>
     )
 }
