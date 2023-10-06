2f2dbfcf56e638030bce24853d6d7567bb744911
jonathanmorenofarias <91215724+jonathanmorenofarias@users.noreply.github.com>
2023-10-05 17:23:59 -0700

    Done. Needs to use URLs with q_good. Maybe modify the past events a bit.

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,62 +1,51 @@
 import React from 'react'
 import Noches from '../data/images/events/noches.png'
-
+import ActivityContent from '../components/event/ActivityContent'
+import { Link } from 'react-router-dom'
 
 const Event = () => {
+  const divStyle = {
+    backgroundImage: 'url(' + require('../data/images/events/newish.jpeg') + ')',
+    backgroundPosition: 'top',
+    backgroundSize: 'cover'
+}
+
+
   return (
+
     <div className=''> 
-      <div className='p-[2rem] flex justify-around items-center'>
-      <div className='flex flex-col gap-[1rem]'>
-      <h1 className='text-[2rem] font-[600] drop-shadow-md'>Events</h1>
-        <p className='w-[30rem]'>SHPE is all about togetherness through exciting events that promote learning, 
+      <div style={divStyle} className='h-[93vh] lg:justify-around lg:flex-row flex flex-col justify-center lg:gap-[1rem] gap-[2rem] items-center shadow-lg px-[2rem]'>
+        <div className='flex flex-col lg:items-start items-center gap-[1rem] lg:text-left text-center'>
+          <h1 className='2xl:text-[2.5rem] lg:text-[2rem] md:text-[2rem] text-[1.5rem] font-[600]'>Events</h1>
+          <p className='2xl:w-[35rem] lg:w-[20rem] md:w-[90vw] 2xl:text-[1.25rem] lg:text-[1rem] md:text-[1.25rem] text-[1rem]'>SHPE is all about togetherness through exciting events that promote learning, 
           connection, and celebrating diversity. Join us to build lasting connections and experience the sense of 
           belonging within our Familia!</p>
+          <button className='w-[15rem] h-[3rem] bg-[#F26433] rounded-lg'><Link to="/calendar">Calendar of Events</Link></button>
+        </div>
+        <img src="https://res.cloudinary.com/dy3nrj03h/image/upload/v1696284020/about/convention_mfmyla.png" 
+          alt="" 
+          className='event-main 2xl:h-[30rem] xl:h-[25rem] lg:h-[22rem] h-[55vw] object-cover rounded-md shadow-md'/>
       </div>
-      <img src={Noches} alt="" className='h-[30rem] rounded-full shadow-md'/>
-    </div>
-      <div className='bg-[#f2f2f2] flex justify-around items-center py-[2rem]'>
-          <div className='flex flex-col items-center justify-center gap-[1rem]'>
-            <img src={Noches} alt="" className='h-[30rem] rounded-full shadow-md'/>
-            <h1 className='text-[2rem] font-[600] drop-shadow-md'>General Meetings</h1>
-          </div>
-          <div className='flex flex-col gap-[1rem]'>
-            <h2 className='text-[1.5rem]'>WorkShops</h2>
-            <p className='w-[30rem]'>Workshops are given by engineering professionals around Oregon who give SHPE students advice to assess and 
-              revise their resumes, interviewing abilities, and elevator pitch. SHPE students will learn the expectations 
-              to obtain a intership or full-time career and their impact as a Hispanic in their field.</p>
-            <h2 className='text-[1.5rem]'>Social Gatherings</h2>
-            <p className='w-[30rem]'>Gatherings alllow us to create friendships and find a community at OSU. We know that being in STEM 
-              and being a Hispanic is rare so getting together to express our cultural values and experiences allow us to create long-lasting 
-              relationships.</p>
-          </div>
-      </div>
-      <div className='flex justify-around items-center py-[2rem]'>
-          <div className='flex flex-col gap-[1rem]'>
-            <h2 className='text-[1.5rem]'>Why we do it?</h2>
-            <p className='w-[30rem]'>Taking OSU students to SHPE's National Conference is one of the main goals of OSU SHPE. Many students don't 
-            have the funds to attend the event, so we use fundraisers to help involved SHPE members with financial need.</p>
-            <h2 className='text-[1.5rem]'>What we do?</h2>
-            <p className='w-[30rem]'>We raise money by conducting fundraiser events at OSU. For example, salsa nights, raffle baskets, selling
-             raspados, etc.</p>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-[1rem]'>
-            <img src={Noches} alt="" className='h-[30rem] rounded-full shadow-md'/>
-            <h1 className='text-[2rem] font-[600] drop-shadow-md'>Fundraisers</h1>
-          </div>
-      </div>
-      <div className='bg-[#f2f2f2] flex justify-around items-center py-[2rem]'>
-          <h1 className='text-[2rem] '>Outreach</h1>
-          <div className='flex flex-col gap-[1rem]'>
-            <h2 className='text-[1.5rem]'>Why we do it?</h2>
-            <p className='w-[30rem]'>We understand that sparking interest in STEM for young Hispanic will create leaders of the future. By doing outreach 
-            events, we will be able increase Hispanic STEM college undergraduates and provide them with the benefits and opportunites that STEM offers.</p>
-            <h2 className='text-[1.5rem]'>What we do?</h2>
-            <p className='w-[30rem]'>We conduct fun outreach events at elementary school and at OSU. For example, Noches de Ciencas, Juntos SHPE 
-            stand, etc.</p>
-          </div>
-      </div>
-      <div className='flex justify-center items-center py-[2rem]'>
-        <h1 className='text-[2rem]'>Past Events</h1>
+
+      <ActivityContent/>
+      
+      
+      <div className='flex flex-col justify-center items-center px-[2rem] py-[3rem] gap-[4rem]'>
+        <h1 className='2xl:text-[2.5rem] lg:text-[2rem] md:text-[2rem] text-[1.5rem] font-[600]'>Past Events</h1>
+        <div className='grid 2xl:grid-cols-4 lg:grid-cols-2 grid-cols-1 gap-[2rem]'>
+          <img src="https://res.cloudinary.com/dy3nrj03h/image/upload/v1696306347/events/gala_1_uz97kw.png" 
+          alt=""
+          className='2xl:h-[30rem] 2xl:w-[23rem] h-[40rem] w-[30rem] shadow-2xl'/>
+          <img src="https://res.cloudinary.com/dy3nrj03h/image/upload/v1696306352/events/bowlingEve_1_zdljg8.png" 
+          alt=""
+          className='2xl:h-[30rem] 2xl:w-[23rem] h-[40rem] w-[30rem] object-cover shadow-2xl'/>
+          <img src="https://res.cloudinary.com/dy3nrj03h/image/upload/v1696306344/events/gradschool_1_jvftgd.png" 
+          alt=""
+          className='2xl:h-[30rem] 2xl:w-[23rem] h-[40rem] w-[30rem] object-cover shadow-2xl'/>
+          <img src="https://res.cloudinary.com/dy3nrj03h/image/upload/v1696306340/events/studysesh_1_ofsu35.png" 
+          alt=""
+          className='2xl:h-[30rem] 2xl:w-[23rem] h-[40rem] w-[30rem] object-cover  shadow-2xl'/>
+        </div>
       </div>
 
     </div>
