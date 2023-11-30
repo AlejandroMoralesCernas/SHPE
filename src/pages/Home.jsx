d81c79f311acd67cf1edcc238dbcf81d02952df1
AlejandroMorales2003 <moralea3@oregonstate.edu>
2023-11-30 13:11:29 -0800

    add fundraiser

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LatestMeeting from '../components/home/LatestMeeting'
 import Slider from '../components/home/Slider'
+import Fundraiser from '../components/home/Fundraiser'
 
 const Home = () => {
   return (
     <div>
       <Slider/>
+      <Fundraiser/>
       <LatestMeeting/>
     </div>
   )
